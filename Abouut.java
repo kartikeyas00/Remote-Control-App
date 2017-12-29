5b7943fb16e24d8d1bf06aacb2a23d1556f98913
kartikeyas00 <33379978+kartikeyas00@users.noreply.github.com>
2017-12-29 12:52:50 +0530

    Add files via upload

---
+++ Abouut.java
@@ -0,0 +1,17 @@
+package com.ars.myrccar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Abouut extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	setContentView(R.layout.abouut);
+	}
+	
+	
+
+}
